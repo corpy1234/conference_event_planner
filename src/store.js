a271c3c1bddfc9b61731a7a79bf7f538b80510b6
corpy1234 <corpuss66@gmail.com>
2025-07-22 12:49:57 -0400

    finished Meals Section

--- src/store.js
+++ src/store.js
@@ -2,9 +2,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
 import avReducer from './avSlice';
+import mealsReducer from './mealsSlice';
 
 export default configureStore({
   reducer: {
-    venue: venueReducer, av: avReducer,
+    venue: venueReducer, av: avReducer, meals: mealsReducer,
   },
 });
